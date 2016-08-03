c10f9643582eb6a8193dcbd7d9e32c00465f59cb
Anthony <grant.anthony@gmail.com>
2016-08-03 03:54:26 -0400

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name formAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the formAppApp
+ */
+angular.module('formAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
